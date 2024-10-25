2c3c97caa1a9553249484f87c0876d28da471a9c
Yuta1409 <roger.yan@educ-valadon-limoges.fr>
2024-10-25 15:15:46 +0200

    fix: refactorisation du code

---
+++ src/Controller/PokemonController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Pokemon;
+use App\Repository\PokemonRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class PokemonController extends AbstractController
+{
+    #[Route('/pokemon', name: 'app_pokemon')]
+    public function index(): Response
+    {
+        return $this->render('pokemon/index.html.twig', [
+            'controller_name' => 'PokemonController',
+        ]);
+    }
+
+    #[Route('/pokemon/list', name: 'app_pokemon_list')]
+    public function list(PokemonRepository $pokemonRepository): Response
+    {
+        $pokemons = $pokemonRepository->findAll();
+        return $this->render('pokemon/list.html.twig', [
+            'pokemons' => $pokemons,
+        ]);
+    }
+}
